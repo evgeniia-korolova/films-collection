434abf6f799b4d007572ad88c714484cba6dbd2d
evgeniia-korolova <jkod@ukr.net>
2026-05-01 21:32:02 +0300

    feat: implement light-dark mode, move breadcrumbs to main

--- src/app/layout/components/header/header.ts
+++ src/app/layout/components/header/header.ts
@@ -1,18 +1,21 @@
-import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, input, output, } from '@angular/core';
 import { FilmService } from '../../../data-access/film-service';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { Breadcrumbs } from "../breadcrumbs/breadcrumbs";
+
+import { ThemeService } from '../../../core/services/theme-service';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive, Breadcrumbs],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Header {
   protected readonly filmService = inject(FilmService);
-  protected readonly title = signal('films-collection');
+  themeService = inject(ThemeService);
+  readonly currentTheme = input.required<string>();
+  toggleTheme = output<void>();  
 
   favoritesCount = computed(() => this.filmService.favoriteFilms().length);
 }
--- src/app/layout/layout/layout.ts
+++ src/app/layout/layout/layout.ts
@@ -1,13 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject } from '@angular/core';
 import { Header } from "../components/header/header";
 import { RouterOutlet } from "@angular/router";
 import { Footer } from "../components/footer/footer";
+import { ThemeService } from '../../core/services/theme-service';
+import { Breadcrumbs } from "../components/breadcrumbs/breadcrumbs";
 
 @Component({
   selector: 'app-layout',
-  imports: [Header, RouterOutlet, Footer],
+  imports: [Header, RouterOutlet, Footer, Breadcrumbs],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Layout {}
+export class Layout {
+  protected readonly themeService = inject(ThemeService);
+  protected readonly currentTheme = this.themeService.theme;
+
+  constructor() {
+    const saved = localStorage.getItem('theme') as 'light' | 'dark' | null;
+    const initial: 'light' | 'dark' = saved === 'dark' ? 'dark' : 'light';
+    document.documentElement.classList.add(initial);
+    this.currentTheme.set(initial);
+  }
+
+  toggleTheme() {
+    const next = this.currentTheme() === 'light' ? 'dark' : 'light';
+    document.documentElement.classList.remove(this.currentTheme());
+    document.documentElement.classList.add(next);
+    this.currentTheme.set(next);
+    localStorage.setItem('theme', next);
+  }
+}
